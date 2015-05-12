9fea88b470fccdfe34b9865c626008bccc619aac
Michal Dziekonski <glibnes@gmail.com>
2015-05-12 15:35:18 +0200

    Create a simple agent app example

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <system_error>
+
+#include "network/bsdsocket/Manager.hpp"
 
 int main()
 {
+    tin::controllers::main::ControllerQueue ctrlQueue;
+    tin::network::bsdsocket::ManagerQueue netManagerQueue;
+    tin::network::bsdsocket::Manager networkManager(netManagerQueue, ctrlQueue, 3333);
+
     std::cout << "Hello agent!\n";
+
+    auto netManager = networkManager.createThread();
+
+    try
+    {
+        netManager.join();
+    }
+    catch (std::system_error& e)
+    {
+        // Could not join one of the threads
+    }
 
     return 0;
 }
