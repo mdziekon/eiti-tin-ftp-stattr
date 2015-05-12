056bb998ccb30dba0a8f47b547c2f29c7b29acd3
Michal Dziekonski <glibnes@gmail.com>
2015-05-12 17:25:46 +0200

    Let supervisor send something to the agent

--- supervisor/src/main.cpp
+++ supervisor/src/main.cpp
@@ -2,20 +2,38 @@
 #include <system_error>
 
 #include "network/websocket/Manager.hpp"
+#include "network/bsdsocket/Manager.hpp"
+
+#include "network/bsdsocket/events/MessageRequest.hpp"
 
 int main()
 {
     tin::controllers::main::ControllerQueue ctrlQueue;
     tin::network::websocket::ManagerQueue netManagerQueue;
     tin::network::websocket::Manager networkManager(netManagerQueue, ctrlQueue, 9001);
+    tin::network::bsdsocket::ManagerQueue bsdManagerQueue;
+    tin::network::bsdsocket::Manager bsdManager(bsdManagerQueue, ctrlQueue);
 
     std::cout << "Hello supervisor!\n";
 
     auto netManager = networkManager.createThread();
+    auto bsdManagerThread = bsdManager.createThread();
+
+    bsdManagerQueue.push(
+        tin::network::bsdsocket::EventPtr(
+            new tin::network::bsdsocket::events::MessageRequest(
+                "localhost",
+                3333,
+                "testmessage",
+                false
+            )
+        )
+    );
 
     try
     {
         netManager.join();
+        bsdManagerThread.join();
     }
     catch (std::system_error& e)
     {
