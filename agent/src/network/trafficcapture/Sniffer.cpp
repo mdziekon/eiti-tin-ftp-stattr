c53958613b54a928e31f70751b6a233c1974189d
Michal Dziekonski <glibnes@gmail.com>
2015-05-17 17:52:57 +0200

    Use trampoline function to match C-style callbacks

--- agent/src/network/trafficcapture/Sniffer.cpp
+++ agent/src/network/trafficcapture/Sniffer.cpp
@@ -2,87 +2,10 @@
 
 using namespace tin::network::trafficcapture;
 
-std::vector<std::shared_ptr<tin::utils::Packet>> SnifferProxy::packetVector;
-std::mutex SnifferProxy::packetMutex;
-std::condition_variable SnifferProxy::packetVectorEmpty;
-
-void SnifferProxy::gotPacket(u_char *user, const struct pcap_pkthdr *header, const u_char *packet)
+void pcap_trampoline(u_char *param, const struct pcap_pkthdr *pkt_header, const u_char *pkt_data)
 {
-	static int counter = 0;                   // packet counter 
-    
-    // declare pointers to packet headers 
-    const struct ethernet_header *ethernet;      // The ethernet header [1] 
-    const struct ip_header *ip;             // The IP header 
-    const struct tcp_header *tcp;           // The TCP header 
-    const struct pcaprec_header *pcap;      // The PCAP record header
-
-    int size_ip;
-    int size_tcp;
-    int size_payload;
-    
-    counter++;
-    
-    // define ethernet header
-    ethernet = (struct ethernet_header*)(packet);
-    pcap = (struct pcaprec_header*)(packet);
-
-    long date = (long) pcap->ts_sec;
-    time_t timestamp = time(&date);
-    
-    // define/compute ip header offset 
-    ip = (struct ip_header*)(packet + SIZE_ETHERNET);
-    size_ip = IP_HL(ip)*4;
-    if (size_ip < 20) 
-    {
-        printf("   * Invalid IP header length: %u bytes\n", size_ip);
-        return;
-    }
-
-    // if it is not TCP 
-    if(ip->ip_pro != IPPROTO_TCP)
-    {
-        auto pac = std::make_shared<tin::utils::Packet>(new tin::utils::Packet(counter, timestamp, ip->ip_src, ip->ip_dst, ip->ip_pro));
-        packetVector.push_back(pac);
-        pac->showPacketInfo();
-        return;
-    }
-    
-    // If it is TCP - we have some extra knowledge
-    
-    // Tcp header offset
-    tcp = (struct tcp_header*)(packet + SIZE_ETHERNET + size_ip);
-    size_tcp = TH_OFF(tcp)*4;
-    if (size_tcp < 20) 
-    {
-        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
-        return;
-    } 
-    
-    // compute tcp payload (segment) size 
-    size_payload = ntohs(ip->ip_len) + (size_ip + size_tcp);
-    
-    // create new "Packet" and add it to vector
-    auto pac = std::make_shared<tin::utils::Packet>(new tin::utils::Packet(counter, timestamp, ip->ip_src, ip->ip_dst, ip->ip_pro, tcp->th_sport, tcp->th_dport, size_payload));
-    SnifferProxy::pushPacket(pac);
-    pac->showPacketInfo();
-}
-
-void SnifferProxy::pushPacket(const std::shared_ptr<tin::utils::Packet>& packet)
-{
-    std::unique_lock<std::mutex> lock(SnifferProxy::packetMutex);
-    SnifferProxy::packetVector.push_back(packet);
-    if(packetVector.size() == 1) {
-        SnifferProxy::packetVectorEmpty.notify_all();
-    }
-}
-
-std::shared_ptr< tin::utils::Packet > SnifferProxy::pollPacket()
-{
-    std::unique_lock<std::mutex> lock(SnifferProxy::packetMutex);
-    SnifferProxy::packetVectorEmpty.wait(lock);
-    auto packet = SnifferProxy::packetVector.back();
-    SnifferProxy::packetVector.pop_back();
-    return packet;
+    Sniffer* obj = reinterpret_cast<Sniffer*>(param);
+    obj->handlePacket(pkt_header, pkt_data);
 }
 
 Sniffer::Sniffer(const std::string& device, const std::string& expression):
@@ -92,29 +15,28 @@ expression(expression)
 
 void Sniffer::sniff()
 {
-    char errbuf[PCAP_ERRBUF_SIZE]; // error buffer 
-    pcap_t *handle;                // packet capture handle 
+    char errbuf[PCAP_ERRBUF_SIZE]; // error buffer
+    pcap_t *handle;                // packet capture handle
 
-    struct bpf_program fp;         // compiled filter program (expression) 
-    bpf_u_int32 mask;              // subnet mask 
-    bpf_u_int32 net;               // ip 
+    struct bpf_program fp;         // compiled filter program (expression)
+    bpf_u_int32 mask;              // subnet mask
+    bpf_u_int32 net;               // ip
     int num_packets = -1;          // number of packets to capture (-1 means till error) */
 
 
     // get device
-    auto dev = getDevice(device);
+    auto dev = this->device.c_str();
 
-
-    if (dev == NULL) 
+    if (dev == NULL)
     {
         fprintf(stderr, "Couldn't find default device: %s\n",
             errbuf);
         exit(EXIT_FAILURE);
     }
-    
-    
-    // get network number and mask associated with capture device 
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) 
+
+
+    // get network number and mask associated with capture device
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
     {
         fprintf(stderr, "Couldn't get netmask for device %s: %s\n",
             dev, errbuf);
@@ -123,48 +45,48 @@ void Sniffer::sniff()
     }
 
     // get an expression
-    filter = getExpression(expression);
+    auto filter = this->expression.c_str();
 
-    // print info 
+    // print info
     printf("Device: %s\n", dev);
     printf("Number of packets: %d\n", num_packets);
-    printf("Filter expression: %s\n", filter.c_str());
+    printf("Filter expression: %s\n", filter);
 
     // open capture device
     handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
-    if (handle == NULL) 
+    if (handle == NULL)
     {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         exit(EXIT_FAILURE);
     }
 
     // make sure we're capturing on an Ethernet device [2]
-    if (pcap_datalink(handle) != DLT_EN10MB) 
+    if (pcap_datalink(handle) != DLT_EN10MB)
     {
         fprintf(stderr, "%s is not an Ethernet\n", dev);
         exit(EXIT_FAILURE);
     }
 
-    // compile the filter 
-    if (pcap_compile(handle, &fp, filter.c_str(), 0, net) == -1) 
+    // compile the filter
+    if (pcap_compile(handle, &fp, filter, 0, net) == -1)
     {
         fprintf(stderr, "Couldn't parse filter %s: %s\n",
-            filter.c_str(), pcap_geterr(handle));
+            filter, pcap_geterr(handle));
         exit(EXIT_FAILURE);
     }
 
-    // apply the filter 
-    if (pcap_setfilter(handle, &fp) == -1) 
+    // apply the filter
+    if (pcap_setfilter(handle, &fp) == -1)
     {
         fprintf(stderr, "Couldn't install filter %s: %s\n",
-            filter.c_str(), pcap_geterr(handle));
+            filter, pcap_geterr(handle));
         exit(EXIT_FAILURE);
     }
 
     // capturing packets
-    pcap_loop(handle, num_packets, SnifferProxy::gotPacket, NULL);
+    pcap_loop(handle, num_packets, pcap_trampoline, reinterpret_cast<u_char*>(this));
 
-    // cleanup 
+    // cleanup
     pcap_freecode(&fp);
     pcap_close(handle);
 
@@ -172,19 +94,67 @@ void Sniffer::sniff()
 
 }
 
-
-
-const char* Sniffer::getDevice(const std::string& device)
+void Sniffer::handlePacket(const struct pcap_pkthdr *header, const u_char *packet)
 {
-   char* dev;
-   dev = strdup(device.c_str());
-   return dev;
-}
+    // declare pointers to packet headers
+    const struct ethernet_header *ethernet;      // The ethernet header [1]
+    const struct ip_header *ip;             // The IP header
+    const struct tcp_header *tcp;           // The TCP header
+    const struct pcaprec_header *pcap;      // The PCAP record header
 
-const char* Sniffer::getExpression(const std::string& expression)
-{
-   this->expression = strdup(device.c_str());
-   return expression.c_str();
+    int size_ip;
+    int size_tcp;
+    int size_payload;
+
+    this->packetCounter++;
+
+    // define ethernet header
+    ethernet = (struct ethernet_header*)(packet);
+    pcap = (struct pcaprec_header*)(packet);
+
+    long date = (long) pcap->ts_sec;
+    time_t timestamp = time(&date);
+
+    // define/compute ip header offset
+    ip = (struct ip_header*)(packet + SIZE_ETHERNET);
+    size_ip = IP_HL(ip)*4;
+    if (size_ip < 20)
+    {
+        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+        return;
+    }
+
+    // if it is not TCP
+    if(ip->ip_pro != IPPROTO_TCP)
+    {
+        auto pac = std::make_shared<tin::utils::Packet>(new tin::utils::Packet(this->packetCounter, timestamp, ip->ip_src, ip->ip_dst, ip->ip_pro));
+
+        this->runPacketReceivedHandlers(pac);
+
+        pac->showPacketInfo();
+        return;
+    }
+
+    // If it is TCP - we have some extra knowledge
+
+    // Tcp header offset
+    tcp = (struct tcp_header*)(packet + SIZE_ETHERNET + size_ip);
+    size_tcp = TH_OFF(tcp)*4;
+    if (size_tcp < 20)
+    {
+        printf("   * Invalid TCP header length: %u bytes\n", size_tcp);
+        return;
+    }
+
+    // compute tcp payload (segment) size
+    size_payload = ntohs(ip->ip_len) + (size_ip + size_tcp);
+
+    // create new "Packet" and add it to vector
+    auto pac = std::make_shared<tin::utils::Packet>(new tin::utils::Packet(this->packetCounter, timestamp, ip->ip_src, ip->ip_dst, ip->ip_pro, tcp->th_sport, tcp->th_dport, size_payload));
+
+    this->runPacketReceivedHandlers(pac);
+
+    pac->showPacketInfo();
 }
 
 void Sniffer::run()
@@ -195,6 +165,14 @@ void Sniffer::run()
     );
 
     this->snifferThread.detach();
+}
+
+void Sniffer::runPacketReceivedHandlers(const tin::utils::Packet::ptr& packetPtr)
+{
+    for (auto iter: this->packetReceivedHandlers)
+    {
+        iter.second(packetPtr);
+    }
 }
 
 unsigned int Sniffer::attachPacketReceivedHandler(std::function<void(const tin::utils::Packet::ptr&)>& handler)
--- agent/src/network/trafficcapture/Sniffer.hpp
+++ agent/src/network/trafficcapture/Sniffer.hpp
@@ -1,7 +1,7 @@
 #ifndef TIN_NETWORK_TRAFFICCAPTURE_SNIFFER_HPP
 #define TIN_NETWORK_TRAFFICCAPTURE_SNIFFER_HPP
 
-#include<pcap.h>
+#include <pcap.h>
 #include <pcap/pcap.h>
 #include <stdio.h>
 #include <string.h>
@@ -26,7 +26,7 @@
 #include "../../utils/packet.h"
 #include "../../utils/HandlersContainer.hpp"
 
-namespace tin { namespace network { namespace trafficcapture 
+namespace tin { namespace network { namespace trafficcapture
 {
     // Max Packet Size
     #define SNAP_LEN 1518
@@ -38,7 +38,7 @@ namespace tin { namespace network { namespace trafficcapture
     #define ETHER_ADDR_LEN  6
 
     // Ethernet header
-    typedef struct ethernet_header 
+    typedef struct ethernet_header
     {
             u_char  ethernet_dhost[ETHER_ADDR_LEN];    // destination
             u_char  ethernet_shost[ETHER_ADDR_LEN];    // source
@@ -46,20 +46,20 @@ namespace tin { namespace network { namespace trafficcapture
     } eth_t;
 
     /* IP header */
-    typedef struct ip_header 
+    typedef struct ip_header
     {
             u_char  ip_vhl;                 // version
             u_char  ip_tos;                 // type of service
             u_short ip_len;                 // total length
             u_short ip_id;                  // id
             u_short ip_offset;              // offset
-            #define IP_RF 0x8000            // reserved fragment flag 
-            #define IP_DF 0x4000            // dont fragment flag 
-            #define IP_MF 0x2000            // more fragments flag 
-            #define IP_OFFMASK 0x1fff       // mask for fragmenting bits 
-            u_char  ip_ttl;                 // time to live 
-            u_char  ip_pro;                 // protocol 
-            u_short ip_checksum;            // checksum 
+            #define IP_RF 0x8000            // reserved fragment flag
+            #define IP_DF 0x4000            // dont fragment flag
+            #define IP_MF 0x2000            // more fragments flag
+            #define IP_OFFMASK 0x1fff       // mask for fragmenting bits
+            u_char  ip_ttl;                 // time to live
+            u_char  ip_pro;                 // protocol
+            u_short ip_checksum;            // checksum
             struct  in_addr ip_src, ip_dst; // source and dest address
     } ip_t;
 
@@ -69,12 +69,12 @@ namespace tin { namespace network { namespace trafficcapture
     // TCP Header
     typedef u_int tcp_seq;
 
-typedef struct tcp_header 
+typedef struct tcp_header
 {
-        u_short th_sport;               // source port 
-        u_short th_dport;               // destination port 
-        tcp_seq th_seq;                 // sequence number 
-        tcp_seq th_ack;                 // acknowledgement number 
+        u_short th_sport;               // source port
+        u_short th_dport;               // destination port
+        tcp_seq th_seq;                 // sequence number
+        tcp_seq th_ack;                 // acknowledgement number
         u_char  th_offx2;               // data offset
         #define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
         u_char  th_flags;
@@ -87,33 +87,18 @@ typedef struct tcp_header
         #define TH_ECE  0x40
         #define TH_CWR  0x80
         #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
-        u_short th_win;                 // window 
-        u_short th_sum;                 // checksum 
-        u_short th_urp;                 // urgent pointer 
+        u_short th_win;                 // window
+        u_short th_sum;                 // checksum
+        u_short th_urp;                 // urgent pointer
 } tcp_t;
 
-    typedef struct pcaprec_header 
+    typedef struct pcaprec_header
     {
-            u_int32_t ts_sec;         // timestamp seconds 
-            u_int32_t ts_usec;        // timestamp microseconds 
-            u_int32_t incl_len;       // number of octets of packet saved in file 
-            u_int32_t orig_len;       // actual length of packet 
-    } pcaprec_t; 
-
-    /* A singleton with shared state for C-style event handlers */
-    class SnifferProxy
-    {
-        static std::vector<std::shared_ptr<tin::utils::Packet>> packetVector;
-        static std::atomic<int> totalClients;
-        static std::atomic<int> servedClients;
-
-        static std::mutex packetMutex;
-        static std::condition_variable packetVectorEmpty;
-    public:
-        static void gotPacket(u_char *user, const struct pcap_pkthdr *header, const u_char *packet);
-        static void pushPacket(const std::shared_ptr<tin::utils::Packet>& packet);
-        static std::shared_ptr<tin::utils::Packet> pollPacket();
-    };
+            u_int32_t ts_sec;         // timestamp seconds
+            u_int32_t ts_usec;        // timestamp microseconds
+            u_int32_t incl_len;       // number of octets of packet saved in file
+            u_int32_t orig_len;       // actual length of packet
+    } pcaprec_t;
 
     class Sniffer
     {
@@ -122,21 +107,21 @@ typedef struct tcp_header
 
         void sniff();
 
-        const char* getDevice(const std::string& device);
-        const char* getExpression(const std::string& expression);
+        void handlePacket(const struct pcap_pkthdr *header, const u_char *packet);
         void run();
         unsigned int attachPacketReceivedHandler(std::function<void(const tin::utils::Packet::ptr&)>& handler);
         unsigned int attachPacketReceivedHandler(std::function<void(const tin::utils::Packet::ptr&)>&& handler);
 
     private:
         std::string device;
-        std::string filter;
         std::string expression;
-        static std::vector<tin::utils::Packet*> packetVector;
+
+        unsigned int packetCounter = 0;
 
         std::thread snifferThread;
 
         tin::utils::HandlersContainer<void(const tin::utils::Packet::ptr&)> packetReceivedHandlers;
+        void runPacketReceivedHandlers(const tin::utils::Packet::ptr& packetPtr);
     };
 }}}
 
