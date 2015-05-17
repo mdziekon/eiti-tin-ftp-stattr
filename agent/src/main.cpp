f70bad5dc97d7a4a2873e508887f4ad925d8cb0c
Michal Dziekonski <glibnes@gmail.com>
2015-05-17 18:46:54 +0200

    Create a simple example of sniffing

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -3,6 +3,7 @@
 
 #include "controllers/main/MainModule.hpp"
 #include "network/bsdsocket/Manager.hpp"
+#include "network/sniffer/SnifferManager.hpp"
 
 int main()
 {
@@ -11,6 +12,7 @@ int main()
 
     tin::controllers::main::MainModule mainCtrl(ctrlQueue, netManagerQueue);
     tin::network::bsdsocket::Manager networkManager(netManagerQueue, ctrlQueue, 3333);
+    tin::network::sniffer::SnifferManager sniffManager(ctrlQueue, "lo", "src 127.0.0.1");
 
     std::cout << "Hello agent!\n";
 
