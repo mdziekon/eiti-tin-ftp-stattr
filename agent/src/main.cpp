3983c5b96e415fe8b1bc9a24161c8939c8332064
Michal Dziekonski <glibnes@gmail.com>
2015-05-21 23:48:52 +0200

    Add a simple example of sniffer manager

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -3,26 +3,45 @@
 
 #include "controllers/main/MainModule.hpp"
 #include "network/bsdsocket/Manager.hpp"
-#include "network/sniffer/SnifferManager.hpp"
+#include "network/sniffer/Manager.hpp"
+
+#include "network/sniffer/events/ChangeFilter.hpp"
 
 int main()
 {
     tin::controllers::main::ControllerQueue ctrlQueue;
     tin::network::bsdsocket::ManagerQueue netManagerQueue;
+    tin::network::sniffer::ManagerQueue snifferManagerQueue;
 
     tin::controllers::main::MainModule mainCtrl(ctrlQueue, netManagerQueue);
     tin::network::bsdsocket::Manager networkManager(netManagerQueue, ctrlQueue, 3333);
-    tin::network::sniffer::SnifferManager sniffManager(ctrlQueue, "lo", "src 127.0.0.1");
+    tin::network::sniffer::Manager sniffManager(
+        snifferManagerQueue,
+        ctrlQueue,
+        "lo",
+        "src 127.0.0.1"
+    );
 
     std::cout << "Hello agent!\n";
 
     auto mainCtrlThread = mainCtrl.createThread();
     auto netManager = networkManager.createThread();
+    auto sniffThread = sniffManager.createThread();
+
+    std::this_thread::sleep_for (std::chrono::seconds(1));
+
+    snifferManagerQueue.push(
+        std::make_shared<tin::network::sniffer::events::ChangeFilter>(
+            "lo",
+            "src 127.0.0.1"
+        )
+    );
 
     try
     {
         mainCtrlThread.join();
         netManager.join();
+        sniffThread.join();
     }
     catch (std::system_error& e)
     {
