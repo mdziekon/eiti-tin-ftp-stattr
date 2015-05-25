473f0d3fdda402fe2bc89b6e1a538e17a9d9a38e
danielobrebski <dani@notebookcheck.pl>
2015-05-25 20:51:24 +0200

    handling new sniffer events

--- agent/src/network/sniffer/ManagerVisitor.cpp
+++ agent/src/network/sniffer/ManagerVisitor.cpp
@@ -4,6 +4,10 @@
 
 #include "events/Terminate.hpp"
 #include "events/ChangeFilter.hpp"
+#include "events/StopSniffing.hpp"
+#include "events/StartSniffing.hpp"
+#include "../../controllers/main/events/SnifferStatus.hpp"
+
 
 #include <iostream>
 
@@ -23,6 +27,31 @@ void ManagerVisitor::visit(events::ChangeFilter& evt)
     this->manager.sniffer.changeConfig(evt.device, evt.expression);
     this->manager.sniffer.run();
 }
+
+void ManagerVisitor::visit(events::StopSniffing& evt)
+{
+    std::cout << "[Sniffer] Stop sniffing" << std::endl;
+
+    this->manager.sniffer.stopSniffing();
+}
+
+void ManagerVisitor::visit(events::StartSniffing& evt)
+{
+    std::cout << "[Sniffer] Start sniffing" << std::endl;
+
+    if(!this->manager.sniffer.isSniffing())
+    	this->manager.sniffer.run();
+}
+
+void ManagerVisitor::visit(events::IsSniffing& evt)
+{
+    std::cout << "[Sniffer] IsSniffing" << std::endl;
+
+    this->manager.controllerQueue.push
+    (tin::controllers::main::EventPtr(new tin::controllers::main::events::SnifferStatus
+    	(this->manager.sniffer.isSniffing())));
+}
+
 
 ManagerVisitor::ManagerVisitor(tin::network::sniffer::Manager& manager):
 manager(manager)
