727c6d3841147fc1ded7109871bcae38855e2abb
danielobrebski <dani@notebookcheck.pl>
2015-05-25 03:30:55 +0200

    handled machine status

--- supervisor/src/controllers/main/MainVisitor.cpp
+++ supervisor/src/controllers/main/MainVisitor.cpp
@@ -1,11 +1,15 @@
 #include "MainVisitor.hpp"
 
 #include <iostream>
+#include <ctime>
+#include "Event.hpp"
+#include "typedefs.hpp"
 
 #include "MainModule.hpp"
 
 #include "events/Terminate.hpp"
 #include "events/CmdResponseReceived.hpp"
+#include "../../utils/Machine.hpp"
 
 #include "../../network/websocket/typedefs.hpp"
 #include "../../network/websocket/events/MessageSendRequest.hpp"
@@ -13,6 +17,8 @@
 #include "../../network/websocket/events/MessageBroadcastRequest.hpp"
 
 namespace events = tin::controllers::main::events;
+namespace main = tin::controllers::main;
+using nlohmann::json;
 
 tin::controllers::main::MainVisitor::MainVisitor(tin::controllers::main::MainModule& controller):
 controller(controller)
@@ -27,4 +33,42 @@ void tin::controllers::main::MainVisitor::visit(events::CmdResponseReceived &evt
 {
     std::cout << "[Supervisor] [MainCtrl] Received response: " << evt.jsonPtr->dump() << std::endl;
     std::cout << "                        Source: " << evt.ip << ":" << evt.port << std::endl;
+    
+
+    auto it = std::make_pair(evt.ip, evt.port);
+    std::map<std::pair<std::string, unsigned int>, int>::iterator ipPortIdMapIt;
+    std::map<int, utils::Machine>::iterator idMachineMapIt;
+    ipPortIdMapIt = controller.ipPortIdMap.find(it);
+    idMachineMapIt = controller.idMachineMap.find(ipPortIdMapIt->second);
+    utils::Machine m = idMachineMapIt->second;
+
+    json& temp = *(evt.jsonPtr);
+
+    if (temp.find("error") == temp.end())
+    {
+    	m.status = "offline";
+        std::cout << "[Supervisor][MainCtrl] Error received" << std::endl;
+        return;
+    }
+
+    const std::string cmd = temp["cmd"];
+
+    if (cmd == "sync")
+    {
+        m.lastSynchronization = time(NULL);
+    }
+
+    else if (cmd == "ping")
+    {
+    	if(m.status == "offline")
+    		m.status.assign("online");
+    }
+
+    else if (cmd == "change_filter")
+    {
+    	const std::string expr = temp["expression"];
+    	m.filter = expr;
+    	m.status = "sniffing";
+    }
+
 }
