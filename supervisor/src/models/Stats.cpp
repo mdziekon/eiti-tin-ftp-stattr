2182af197c2ef4228812976c97626d48c7f339f5
Michal Dziekonski <glibnes@gmail.com>
2015-05-27 01:31:13 +0200

    Fix stats-per-day

--- supervisor/src/models/Stats.cpp
+++ supervisor/src/models/Stats.cpp
@@ -12,93 +12,100 @@ using namespace tin::supervisor::models;
 
 const tin::utils::json::ptr Stats::computeStatsPerDay(const tin::utils::json::ptr& requestorData) const
 {
-    tin::utils::json::ptr reply = std::make_shared<nlohmann::json>(nlohmann::json::object());
-    
-    std::unordered_map<u_int32_t, u_int32_t> machineStats;
-
-    (*reply)["route"] = (*requestorData)["route"];
-    (*reply)["type"] = "GET";
-    (*reply)["uid"] = (*requestorData)["uid"];
-    (*reply)["data"] = { {"stats", nlohmann::json::array() }};
-    
-
-    if(this->packetsByTimestamp.size() > 0)
+    tin::utils::json::ptr reply(new nlohmann::json);
+    try
     {
-        try
-        {
-            struct tm packetDay;
-            int64_t beginTime = this->packetsByTimestamp.begin()->first;
-            struct tm currentDay = *localtime(&beginTime);
-            u_int32_t arrayIndex = 0;
-            for(auto connectionDataPair: this->packetsByTimestamp) 
-            {
-                auto& connectionData = connectionDataPair.second;
-                u_int32_t sourceIP = (*connectionData)["sourceIP"];
-                u_int32_t destinationIP = (*connectionData)["destinationIP"];
-                
-                if(machineStats.find(sourceIP) != machineStats.end()) {
-                    machineStats[sourceIP]++;
-                }
-                else {
-                    machineStats[sourceIP] = 1;
-                }
-                if(machineStats.find(destinationIP) != machineStats.end()) {
-                    machineStats[destinationIP]++;
-                }
-                else {
-                    machineStats[destinationIP] = 1;
-                }
+        std::unordered_map<u_int32_t, u_int32_t> machineStats;
 
-                int64_t temporaryTimeStorage = connectionDataPair.first;
-                packetDay = *localtime(&temporaryTimeStorage);
-                
-                if(packetDay.tm_mday > currentDay.tm_mday ||
-                    packetDay.tm_year > currentDay.tm_year)
-                {
-                    u_int32_t innerMachineID = 0;
-                    
-                    for(auto stat: machineStats) 
-                    {
-                        (*reply)["data"]["stats"][arrayIndex]["day"] = mktime(&currentDay);
-                        (*reply)["data"]["stats"][arrayIndex]["machines"][innerMachineID]["id"] = innerMachineID;
-                        (*reply)["data"]["stats"][arrayIndex]["machines"][innerMachineID]["name"] = stat.first;
-                        (*reply)["data"]["stats"][arrayIndex]["machines"][innerMachineID]["traffic"] = stat.second;
-                        innerMachineID++;
-                    }
-                    currentDay = packetDay;
-                    arrayIndex++;
-                }
-            }
-            u_int32_t innerMachineID = 0;
-            
-            for(auto stat: machineStats) 
-            {
-                (*reply)["data"]["stats"][arrayIndex]["day"] = mktime(&currentDay);
-                (*reply)["data"]["stats"][arrayIndex]["machines"][innerMachineID]["id"] = innerMachineID;
-                (*reply)["data"]["stats"][arrayIndex]["machines"][innerMachineID]["name"] = stat.first;
-                (*reply)["data"]["stats"][arrayIndex]["machines"][innerMachineID]["traffic"] = stat.second;
-                innerMachineID++;
-            }
-        } catch(std::exception& e)
+        (*reply)["route"] = (*requestorData)["route"];
+        (*reply)["type"] = "GET";
+        (*reply)["uid"] = (*requestorData)["uid"];
+        (*reply)["data"] = { {"stats", nlohmann::json::array() }};
+
+        std::unordered_map<int, nlohmann::json> dStats;
+        std::unordered_map<int, std::unordered_map<int, nlohmann::json>> mStats;
+
+        for(auto& it: this->packets)
         {
-            std::cout << "[][][] ERROR IN computeStatsPerDay" << std::endl;
-            (*reply)["data"] = { {"stats", nlohmann::json::array() }};
+            int dayTimestamp = it.timestamp;
+            dayTimestamp -= dayTimestamp % (24 * 60 * 60);
+
+            if (mStats.count(dayTimestamp) == 0)
+            {
+                mStats.insert({ dayTimestamp, std::unordered_map<int, nlohmann::json>() });
+            }
+
+            auto& thisMStats = mStats.at(dayTimestamp);
+
+            if (this->machines.hasMachine(it.getSourceIP(), it.sourcePort))
+            {
+                auto& machine = this->machines.getMachine(it.getSourceIP(), it.sourcePort);
+                if (thisMStats.count(machine.id) == 0)
+                {
+                    thisMStats.insert({ machine.id, nlohmann::json::object() });
+                    thisMStats.at(machine.id) = {
+                        { "id", machine.id },
+                        { "name", machine.name },
+                        { "traffic", {
+                            { "in", 0 },
+                            { "out", 0 }
+                        }}
+                    };
+                }
+                auto& jsObj = thisMStats.at(machine.id);
+
+                jsObj["traffic"]["out"] = (jsObj["traffic"]["out"].get<int>() + it.payloadSize);
+            }
+            else if (this->machines.hasMachine(it.getDestinationIP(), it.destinationPort))
+            {
+                auto& machine = this->machines.getMachine(it.getDestinationIP(), it.destinationPort);
+                if (thisMStats.count(machine.id) == 0)
+                {
+                    thisMStats.insert({ machine.id, nlohmann::json::object() });
+                    thisMStats.at(machine.id) = {
+                        { "id", machine.id },
+                        { "name", machine.name },
+                        { "traffic", {
+                            { "in", 0 },
+                            { "out", 0 }
+                        }}
+                    };
+                }
+                auto& jsObj = thisMStats.at(machine.id);
+
+                jsObj["traffic"]["in"] = (jsObj["traffic"]["in"].get<int>() + it.payloadSize);
+            }
+        }
+
+        for(auto& itt: mStats)
+        {
+            (*reply)["data"]["stats"][(*reply)["data"]["stats"].size()] = {
+                { "day", itt.first },
+                { "machines", nlohmann::json::array() }
+            };
+
+            for(auto& ittt: itt.second)
+            {
+                (*reply)["data"]["stats"][(*reply)["data"]["stats"].size() - 1]["machines"][(*reply)["data"]["stats"][(*reply)["data"]["stats"].size() - 1]["machines"].size()] = ittt.second;
+            }
+
+            // (*reply)["data"]["machines"][(*reply)["data"]["machines"].size()] = itt.second;
         }
     }
-    else {
-        (*reply)["data"] = { {"stats", nlohmann::json::array() }};
+    catch (std::exception& e)
+    {
+        (*reply)["route"] = (*requestorData)["route"];
+        (*reply)["type"] = "GET";
+        (*reply)["uid"] = (*requestorData)["uid"];
+        (*reply)["error"] = { {"unknown", true }};
     }
-
-    // std::cout << "---- COMPUTE1 ---- " << std::endl;
-    // std::cout << (*reply).dump() << std::endl;
-    // std::cout << "---- COMPUTE ---- " << std::endl;
 
     return reply;
 }
 
 const tin::utils::json::ptr Stats::computeIndividualUsage(const tin::utils::json::ptr& requestorData) const
 {
-        tin::utils::json::ptr reply(new nlohmann::json);
+    tin::utils::json::ptr reply(new nlohmann::json);
     try
     {
         std::unordered_map<u_int32_t, u_int32_t> machineStats;
